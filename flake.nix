3f6e06aeb7c63ce114c3e2cb07b6d345ccd9f5a4
Mitchell Mullen <michalmullen@gmail.com>
2024-05-08 11:00:09 +0200

    fix: change nixpkgs url to stable 23.11

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -17,7 +18,7 @@
       specialArgs = {inherit inputs;};
       modules = [
         inputs.home-manager.nixosModules.default
-        inputs.nixos-hardware.nixosModules.framework-13-7040-amd
+        # inputs.nixos-hardware.nixosModules.framework-13-7040-amd
         ./hosts/framework/configuration.nix
       ];
     };
