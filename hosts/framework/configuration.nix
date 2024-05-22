ff97d185406ea480ab4fe3d89c1aed3536f8a5a3
Mitchell Mullen <michalmullen@gmail.com>
2024-05-22 21:03:07 +0200

    add tailscale

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -135,7 +135,9 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+
+  services.tailscale.enable = true;
 
   # Open ports in the firewall.
   networking.firewall = {
