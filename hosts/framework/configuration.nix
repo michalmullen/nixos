156c582772358083c091302fae4ae43321c9e113
Mitchell Mullen <michalmullen@gmail.com>
2024-06-26 17:38:44 +0200

    test

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -17,7 +17,7 @@
 
   boot.initrd.luks.devices."luks-b70cdad2-38a7-4c5a-b8d7-8c72e195a4d9".device = "/dev/disk/by-uuid/b70cdad2-38a7-4c5a-b8d7-8c72e195a4d9";
   networking.hostName = "framework"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
