0d7b94c02175ab7e351eb4f49c9cd03fd9e2f565
Mitchell Mullen <michalmullen@gmail.com>
2024-07-02 22:06:45 +0200

    increase to 5

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -12,7 +12,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
