654c6a75b7b890eb7a5e6c11f81a4c308f603fed
Mitchell Mullen <michalmullen@gmail.com>
2024-12-23 16:40:47 +0100

    feat: add pass

--- modules/default/kde.nix
+++ modules/default/kde.nix
@@ -5,6 +5,9 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+
+  # Enable plasma integration firefox
+  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
 }
