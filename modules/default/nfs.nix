60033380733c4a933265eb389ce187302b3da1ff
michalmullen <michalmullen@gmail.com>
2025-08-24 10:04:34 +0200

    added nfs module

---
+++ modules/default/nfs.nix
@@ -0,0 +1,17 @@
+{ pkgs }:
+
+{
+  environment.systemPackages = with pkgs; [
+    nfs-utils
+  ];
+
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [ 111 2049 ];
+    allowedTCPPorts = [ 111 2049 ];
+  };
+
+  networking.networkmanager.enable = true; networking.hosts = { "192.168.15.50" = [ "truenas" ]; };
+
+  fileSystems."/home/po252/nfs" = { device = "truenas:/mnt/storage/storage-share"; fsType = "nfs"; options = [ "x-systemd.automount" "noauto" ]; }; 
+}
