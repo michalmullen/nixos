cd92a316d1afa0c841356ce0d40985a6ab6462dd
po252 <andrew.mitchell.mullen@deutsche-boerse.com>
2026-01-19 09:16:18 +0100

    Add GKE gcloud auth plugin to google-cloud-sdk

--- home/core/packages/default.nix
+++ home/core/packages/default.nix
@@ -13,7 +13,7 @@
     starship
     zoxide
     nnn
-    google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     kubectl
     kustomize
     k9s
